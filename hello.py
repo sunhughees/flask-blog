de5a2c20e269b6ca4827300a5d02bf6155f69f30
sunhughees <sunhughees@sina.cn>
2016-04-15 20:43:20 +0800

    modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, abort
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -18,6 +21,7 @@ def baidu():
     return redirect('http://www.baidu.com')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(debug=True)
+    manager.run()
 
 
