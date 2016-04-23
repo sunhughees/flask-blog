cbbe37b41b036054b57a4e0d1f2ad17817981067
sunhughees <sunhughees@sina.cn>
2016-04-23 19:53:42 +0800

    mod hello.py

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-   return render_template('user.html', name=name)
+    return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
