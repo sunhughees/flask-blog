49679c51e7332a5b699908fa23815132fa26387c
sunhughees <sunhughees@sina.cn>
2016-04-29 20:16:53 +0800

    add

--- falcon/look/app.py
+++ falcon/look/app.py
@@ -6,7 +6,7 @@ api = application = falcon.API()
 storage_path = '/home/sven/Desktop/sundapeng/flask-blog/falcon/look/storage/'
 
 image_collection = images.Collection(storage_path)
-image_item = image.Item(storage_path)
+image_item = images.Item(storage_path)
 
 api.add_route('/images', image_collection)
 api.add_route('/images/(name)', image_item)
