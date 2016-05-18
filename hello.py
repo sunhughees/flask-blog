c23f837949fd4c7285e20808e37d551e9ec755af
sunhughees <sunhughees@sina.cn>
2016-05-18 19:52:06 +0800

    m hello

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config()
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+
+
+print __file__
+print 'os.path:', os.path.dirname(__file__)
+print os.path.abspath(os.path.dirname(__file__))
+
+
