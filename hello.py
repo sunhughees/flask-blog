226626109a0b192ab81dd3fd99ed9b827b25d5ef
sunhughees <sunhughees@sina.cn>
2016-05-18 21:44:33 +0800

    m hello

--- hello.py
+++ hello.py
@@ -10,9 +10,23 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config()
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+Class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+Class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64))
 
 
 @app.route('/')
