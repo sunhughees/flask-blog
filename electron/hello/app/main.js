8d9cbc2de92f62511a78041502f2b51d31f6732a
sunhughees <471490816@qq.com>
2016-05-24 15:26:21 +0800

    m elec

--- electron/hello/app/main.js
+++ electron/hello/app/main.js
@@ -17,3 +17,15 @@ function createWindow() {
 }
 
 app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win == null) {
+        createWindow();
+    }
+});
